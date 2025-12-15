e1dcd2c29031d1878364cf5a598588afa90eb6a4
Kiran45181 <kirannelikanti1818@gmail.com>
2025-12-15 11:46:01 +0530

    Update batch_job.py

--- batch_job.py
+++ batch_job.py
@@ -1,6 +1,6 @@
 from prefect import flow, task
 
-# ----------------- Tasks -----------------
+
 @task
 def extract():
     return "Data Extracted"
@@ -13,23 +13,19 @@ def transform(data):
 def load(data):
     print(f"Loading: {data}")
 
-# ----------------- ETL Flow -----------------
 @flow
 def etl_flow():
     raw = extract()
     processed = transform(raw)
     load(processed)
 
-# ----------------- Post ETL Flow -----------------
 @flow
 def post_etl_flow(job_name: str = "Daily ETL"):
     return f"Running job: {job_name}"
 
-# ----------------- Main Execution -----------------
+
 if __name__ == "__main__":
-    # Run ETL flow
     etl_flow()
 
-    # Run Post ETL flow and capture output
     message = post_etl_flow(job_name="Morning ETL")
-    print(message)  # Optional: see output in console
+    print(message)
