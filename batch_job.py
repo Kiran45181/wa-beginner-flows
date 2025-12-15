54efa026a7222690de83b525f6868d404695d863
Kiran45181 <kirannelikanti1818@gmail.com>
2025-12-15 12:05:07 +0530

    Update batch_job.py

--- batch_job.py
+++ batch_job.py
@@ -1,6 +1,5 @@
 from prefect import flow, task
 
-
 @task
 def extract():
     return "Data Extracted"
@@ -11,21 +10,10 @@ def transform(data):
 
 @task
 def load(data):
-    print(f"Loading: {data}")
+    return f"{data}"  # logs will NOT appear in Prefect Cloud
 
 @flow
-def etl_flow():
+def etl_flow(job_name: str = "Default Job"):
     raw = extract()
     processed = transform(raw)
-    load(processed)
-
-#@flow
-#def post_etl_flow(job_name: str = "Daily ETL"):
-    #return f"Running job: {job_name}"
-
-
-if __name__ == "__main__":
-    etl_flow()
-
-    #message = post_etl_flow(job_name="Morning ETL")
-    #print(message)
+    load(f"{job_name}: {processed}")
